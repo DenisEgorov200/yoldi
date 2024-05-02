9c7846dc8a8f629ca1262725da03049cfd9bc186
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-05-02 21:36:45 +0300

    feat: add layout in accounts page

--- src/pages/accounts/ui/accounts.tsx
+++ src/pages/accounts/ui/accounts.tsx
@@ -1,44 +1,20 @@
 export const Accounts = () => {
   return (
-    <div>
-      <h1>accounts</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
-        nesciunt optio, accusamus esse magni nihil assumenda? Vero ad labore,
-        repellendus totam, perspiciatis autem fuga quis, rem laudantium suscipit
-        tenetur sint. Tempore harum temporibus nulla molestias corrupti
-        provident tempora, porro neque est molestiae. Harum laboriosam dolore
-        nostrum modi molestias incidunt, vel, rerum debitis expedita, repellat
-        nisi minima? Repellendus ab illo fugiat. Id dolores tempora, illum at
-        illo temporibus saepe, molestias fugiat repudiandae perspiciatis quae
-        sunt, vero veritatis provident distinctio totam veniam possimus quam
-        reiciendis odit beatae. Laboriosam nesciunt omnis itaque id. Minima
-        perferendis quis, deleniti alias accusamus voluptatem quasi deserunt?
-        Praesentium saepe sapiente modi sint ipsum inventore ut! Sequi ipsa
-        aliquam distinctio quaerat adipisci labore necessitatibus possimus
-        natus! Quo, omnis eos. Rerum molestias eveniet tenetur eligendi nihil
-        quae. Odit doloribus iure ipsam maiores at laboriosam, a repudiandae,
-        quibusdam dolorum modi ipsa. Atque, recusandae voluptatem. Odit rem,
-        suscipit commodi pariatur doloribus blanditiis. Repellat sequi unde
-        eveniet corporis amet expedita aliquam, error fugiat quasi totam velit,
-        dolores illo voluptatibus, perferendis inventore necessitatibus
-        consequatur? Odit recusandae ut exercitationem iusto in blanditiis
-        veritatis similique reprehenderit? Illum consequuntur perspiciatis
-        repellat, placeat nulla alias voluptatum quis saepe quibusdam odio vero
-        eos eligendi velit commodi provident eius autem repudiandae pariatur
-        tempora accusamus id! Sed officia molestiae deleniti optio? Sint fugit
-        provident quas, sit impedit recusandae possimus molestiae ut saepe,
-        architecto consequatur fugiat, aliquam aut necessitatibus. Alias
-        consectetur, voluptate vel vitae, temporibus ullam repellat quam
-        molestias labore commodi minus. Mollitia, omnis! Magni quas quisquam
-        totam accusantium quis recusandae repellendus dignissimos, officia eum
-        placeat pariatur aperiam vero doloribus fuga nihil tenetur quaerat
-        aliquid. Numquam neque voluptatibus ex, doloremque dolor qui.
-        Reprehenderit sit, necessitatibus sequi dolorum rem facilis error
-        similique impedit ut, fuga quisquam adipisci vel pariatur, deleniti
-        repellat. Minus eius architecto nemo cum et a rem placeat voluptatem
-        optio. Quibusdam?
-      </p>
+    <div className="py-12">
+      <h1 className="mb-7 text-title font-medium">Список аккаунтов</h1>
+      <ul className="flex flex-col border-t border-gray-200">
+        <li className="flex items-center justify-between border-b border-gray-200 py-2.5">
+          <div className="flex items-center gap-5">
+            <div className="flex h-12 w-12 items-center justify-center rounded-full border border-gray-200">
+              <img src="./images/avatar.png" alt="avatar" />
+            </div>
+            <h2>Владислав</h2>
+          </div>
+          <a href="mailto:example@gmail.com" className="text-gray-400">
+            example@gmail.com
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
