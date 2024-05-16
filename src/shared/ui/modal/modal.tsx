ca0335b5f485e16d7f7d662eb52f614a0f091e92
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-05-16 19:58:02 +0300

    fix: change props generic to forwardRef

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,24 +1,21 @@
-import { ReactNode, RefObject } from 'react'
+import { forwardRef, ReactNode } from 'react'
 
-interface Props<T extends HTMLDivElement = HTMLDivElement> {
-  modalRef: RefObject<T | null>
+interface Props {
   children: ReactNode
   className?: string
 }
 
-export const Modal = <T extends HTMLDivElement = HTMLDivElement>({
-  modalRef,
-  children,
-  className,
-}: Props<T>) => {
-  return (
-    <div className="fixed left-0 top-0 z-10 flex h-dvh w-dvw items-center justify-center bg-black/20">
-      <div
-        ref={modalRef as RefObject<T>}
-        className={`flex flex-col rounded border border-gray-200 bg-white p-7 ${className}`}
-      >
-        {children}
+export const Modal = forwardRef<HTMLDivElement, Props>(
+  ({ children, className }, ref) => {
+    return (
+      <div className="fixed left-0 top-0 z-10 flex h-dvh w-dvw items-center justify-center bg-black/20">
+        <div
+          ref={ref}
+          className={`flex flex-col rounded border border-gray-200 bg-white p-7 ${className}`}
+        >
+          {children}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  },
+)
