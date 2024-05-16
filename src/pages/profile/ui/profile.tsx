78f0d09a6b1a10fbabcd7f39fa5a9c7588d6ba0f
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-05-16 20:02:14 +0300

    feat: add ProfileModal

--- src/pages/profile/ui/profile.tsx
+++ src/pages/profile/ui/profile.tsx
@@ -7,7 +7,7 @@ import { Input } from '@shared/ui/input'
 import { Modal } from '@shared/ui/modal'
 import { Textarea } from '@shared/ui/textarea'
 import { useUnit } from 'effector-react'
-import { FormEvent, useRef, useState } from 'react'
+import { FormEvent, forwardRef, useRef, useState } from 'react'
 import {
   $address,
   $description,
@@ -18,22 +18,13 @@ import {
   nameChanged,
 } from '../model'
 
+interface ProfileModalProps {
+  isOpenModal: boolean
+  setIsOpenModal: (value: boolean) => void
+}
+
 export const Profile = () => {
-  const { start: changeProfile, pending } = useUnit(currentProfileMutation)
-
   const [profile, handleTokenExpired] = useUnit([$profile, tokenExpired])
-
-  const [name, address, description, profileId] = useUnit([
-    $name,
-    $address,
-    $description,
-    $token,
-  ])
-  const [handleName, handleAddress, handleDescription] = useUnit([
-    nameChanged,
-    addressChanged,
-    descriptionChanged,
-  ])
 
   const uploadRef = useRef<HTMLInputElement>(null)
 
@@ -41,23 +32,6 @@ export const Profile = () => {
   const [isOpenModal, setIsOpenModal] = useState(false)
 
   useOnClickOutside(modalRef, () => setIsOpenModal(false))
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    changeProfile({
-      body: {
-        name,
-        slug: address,
-        description,
-        password: null,
-        imageId: null,
-        coverId: null,
-      },
-      slug: profileId,
-    })
-
-    setIsOpenModal(false)
-  }
 
   return (
     <>
@@ -112,72 +86,115 @@ export const Profile = () => {
           </div>
         </>
       )}
-      {isOpenModal && (
-        <Modal
-          modalRef={modalRef}
-          className="w-full max-w-[600px] gap-6 max-sm:h-full"
-        >
-          <h2 className="text-title font-medium">Редактировать профиль</h2>
-          <form
-            action=""
-            className="flex h-full flex-col gap-3.5"
-            onSubmit={handleSubmit}
-          >
-            <div className="flex flex-col gap-1">
-              <label
-                htmlFor=""
-                className="text-button font-medium text-gray-400"
-              >
-                Имя
-              </label>
-              <Input type="text" value={name} onValue={handleName} />
-            </div>
-            <div className="flex flex-col gap-1">
-              <label
-                htmlFor=""
-                className="text-button font-medium text-gray-400"
-              >
-                Адрес профиля
-              </label>
-              <AddresInput
-                type="text"
-                value={address}
-                onValue={handleAddress}
-                label="example.com/"
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <label
-                htmlFor=""
-                className="text-button font-medium text-gray-400"
-              >
-                Описание
-              </label>
-              <Textarea
-                name=""
-                id=""
-                className="min-h-36 resize-none"
-                value={description}
-                onValue={handleDescription}
-              />
-            </div>
-            <div className="mt-auto flex items-center gap-2.5">
-              <Button
-                type="submit"
-                label="Отмена"
-                className="w-full border-gray-400"
-                onClick={() => setIsOpenModal(false)}
-              />
-              <Button
-                type="submit"
-                label="Сохранить"
-                className="w-full bg-black text-white hover:bg-black/80"
-                disabled={pending}
-              />
-            </div>
-          </form>
-        </Modal>
-      )}
+      <ProfileModal
+        ref={modalRef}
+        isOpenModal={isOpenModal}
+        setIsOpenModal={setIsOpenModal}
+      />
     </>
   )
 }
+
+const ProfileModal = forwardRef<HTMLDivElement, ProfileModalProps>(
+  ({ isOpenModal, setIsOpenModal }, ref) => {
+    const { start: changeProfile, pending } = useUnit(currentProfileMutation)
+
+    const [name, address, description, profileId] = useUnit([
+      $name,
+      $address,
+      $description,
+      $token,
+    ])
+
+    const [handleName, handleAddress, handleDescription] = useUnit([
+      nameChanged,
+      addressChanged,
+      descriptionChanged,
+    ])
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      changeProfile({
+        body: {
+          name,
+          slug: address,
+          description,
+          password: null,
+          imageId: null,
+          coverId: null,
+        },
+        slug: profileId,
+      })
+
+      setIsOpenModal(false)
+    }
+
+    return (
+      <>
+        {isOpenModal && (
+          <Modal ref={ref} className="w-full max-w-[600px] gap-6 max-sm:h-full">
+            <h2 className="text-title font-medium">Редактировать профиль</h2>
+            <form
+              action=""
+              className="flex h-full flex-col gap-3.5"
+              onSubmit={handleSubmit}
+            >
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor=""
+                  className="text-button font-medium text-gray-400"
+                >
+                  Имя
+                </label>
+                <Input type="text" value={name} onValue={handleName} />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor=""
+                  className="text-button font-medium text-gray-400"
+                >
+                  Адрес профиля
+                </label>
+                <AddresInput
+                  type="text"
+                  value={address}
+                  onValue={handleAddress}
+                  label="example.com/"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor=""
+                  className="text-button font-medium text-gray-400"
+                >
+                  Описание
+                </label>
+                <Textarea
+                  name=""
+                  id=""
+                  className="min-h-36 resize-none"
+                  value={description}
+                  onValue={handleDescription}
+                />
+              </div>
+              <div className="mt-auto flex items-center gap-2.5">
+                <Button
+                  type="submit"
+                  label="Отмена"
+                  className="w-full border-gray-400"
+                  onClick={() => setIsOpenModal(false)}
+                />
+                <Button
+                  type="submit"
+                  label="Сохранить"
+                  className="w-full bg-black text-white hover:bg-black/80"
+                  disabled={pending}
+                />
+              </div>
+            </form>
+          </Modal>
+        )}
+      </>
+    )
+  },
+)
